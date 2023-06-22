5f9a5d44ebdff86d7caa965e8f87e8ce4d9fc39c
pufinton-pixie <kg6092@srmist.edu.in>
2023-06-22 10:03:53 +0530

    .

--- src/app/components/project-list/project-list.component.ts
+++ src/app/components/project-list/project-list.component.ts
@@ -4,15 +4,15 @@ import { project } from 'src/app/models/project';
 import { projectService } from 'src/app/services/project.service';
 
 
-
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.css'],
-  providers: [projectService]
+  providers:[projectService]
 })
 export class projectListComponent implements OnInit {
   projects: any = [];
+  selectedproject: project | null = null;
 
   constructor(private projectService: projectService, private router: Router) { }
 
@@ -23,23 +23,54 @@ export class projectListComponent implements OnInit {
   getprojects() {
     this.projectService.getprojects().subscribe(
       (response: any) => {
-        this.projects = response;
-        console.log(response.data)
+        this.projects = response.data;
+        console.log(response);
       }
     );
   }
 
-  editproject(project: project) {
-    this.router.navigate(['/projects', project.Id, 'edit']);
+  createproject() {
+    const newProject: project = {
+      // Set initial values for the new project
+      Id: 0,
+      Title: '',
+      Date: '',
+      Name: '',
+      Description: '',
+      StatusId: 0,
+      BranchId: 0,
+      Sapnumber: 0,
+      Notes: '',
+      ServiceId: '',
+      ServiceName: ''
+    };
+
+    this.projectService.createproject(newProject).subscribe(
+      (response: any) => {
+        this.projects = response.data;
+        console.log('New project created:', newProject);
+      }
+    );
   }
 
-  deleteproject(project: project) {
-    if (confirm('Are you sure you want to delete this project?')) {
-      this.projectService.deleteproject(project.Id).subscribe(
+  editproject() {
+    if (this.selectedproject) {
+      this.router.navigate(['/projects', this.selectedproject.Id, 'edit']);
+    }
+  }
+
+  deleteProject() {
+    if (this.selectedproject && confirm('Are you sure you want to delete this project?')) {
+      this.projectService.deleteproject(this.selectedproject.Id).subscribe(
         () => {
+          console.log('Project deleted:', this.selectedproject);
           this.getprojects();
         }
       );
     }
+  }
+
+  selectproject(project: project) {
+    this.selectedproject = project;
   }
 }
